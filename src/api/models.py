ab1ec5596b70d6ac013b8ee0cd597132930b8aaf
Adrián Araque <106025865+adrarom@users.noreply.github.com>
2024-06-14 09:45:51 +0000

    commit

--- src/api/models.py
+++ src/api/models.py
@@ -22,6 +22,8 @@ class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     type = db.Column(db.String(120), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    level = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
