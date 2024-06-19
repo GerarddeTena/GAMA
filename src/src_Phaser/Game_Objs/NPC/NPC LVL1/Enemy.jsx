ea5cfb13c9bd4d9dace44e4444ade090bfe86747
Adrián Araque <106025865+adrarom@users.noreply.github.com>
2024-06-19 08:56:22 +0000

    added: Enemy.jsx

---
+++ src/src_Phaser/Game_Objs/NPC/NPC LVL1/Enemy.jsx
@@ -0,0 +1,27 @@
+export class Enemy extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frames, points = 10, health = 3) {
+        super(scene, x, y, texture, frames);
+        this.scene = scene;
+        this.points = points;
+        this.health = health;
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+    }
+
+    takeDamage() {
+        this.health -= 1;
+        if (this.health <= 0) {
+            this.die();
+        }
+    }
+
+    die() {
+        this.addPoints();
+        this.destroy();
+    }
+
+    addPoints() {
+        this.scene.score += this.points;
+    }
+}
