7a7efe3eb3d4ef0de565db210221c97ce38c28a6
GERARD_DE_TENA <gerarddtm@gmail.com>
2024-07-04 15:42:53 +0200

    vite.config copied src/assets/**/* --> public // public/**/* --> dist/assets

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import {copy} from 'vite-plugin-copy';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    hmr: true,
-  }
-})
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'src/public/**/*', dest: 'dist/assets' }
+      ],
+      hook: 'writeBundle'
+    })
+  ]
+});
